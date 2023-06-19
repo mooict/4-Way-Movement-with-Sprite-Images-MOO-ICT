ce0850b5cc785af24fe956b83e7e25f47cd7e402
anharali00 <44951121+anharali00@users.noreply.github.com>
2023-06-19 10:22:14 +0100

    Add project files.

---
+++ 4 Way Movement with Sprite Images MOO ICT/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace _4_Way_Movement_with_Sprite_Images_MOO_ICT
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblMovement = new System.Windows.Forms.Label();
+            this.movementTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblCollected = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMovement
+            // 
+            this.lblMovement.AutoSize = true;
+            this.lblMovement.BackColor = System.Drawing.Color.Transparent;
+            this.lblMovement.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMovement.ForeColor = System.Drawing.Color.Coral;
+            this.lblMovement.Location = new System.Drawing.Point(10, 563);
+            this.lblMovement.Name = "lblMovement";
+            this.lblMovement.Size = new System.Drawing.Size(151, 32);
+            this.lblMovement.TabIndex = 0;
+            this.lblMovement.Text = "Movement: ";
+            // 
+            // movementTimer
+            // 
+            this.movementTimer.Enabled = true;
+            this.movementTimer.Interval = 20;
+            this.movementTimer.Tick += new System.EventHandler(this.TimerEvent);
+            // 
+            // lblCollected
+            // 
+            this.lblCollected.BackColor = System.Drawing.Color.Transparent;
+            this.lblCollected.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCollected.ForeColor = System.Drawing.Color.Coral;
+            this.lblCollected.Location = new System.Drawing.Point(397, 563);
+            this.lblCollected.Name = "lblCollected";
+            this.lblCollected.Size = new System.Drawing.Size(437, 32);
+            this.lblCollected.TabIndex = 1;
+            this.lblCollected.Text = "Collected: 00";
+            this.lblCollected.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 611);
+            this.Controls.Add(this.lblCollected);
+            this.Controls.Add(this.lblMovement);
+            this.Name = "Form1";
+            this.Text = "4 Way Sprite Animation  MOO ICT";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormPaintEvent);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblMovement;
+        private System.Windows.Forms.Timer movementTimer;
+        private Label lblCollected;
+    }
+}
---
+++ 4 Way Movement with Sprite Images MOO ICT/Items.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+// created by MOOICT.COM 2023
+// for educational purpose only
+
+namespace _4_Way_Movement_with_Sprite_Images_MOO_ICT
+{
+    internal class Items
+    {
+        public int positionX;
+        public int positionY;
+        public Image item_image;
+        public int height;
+        public int width;
+        public string name;
+
+        Random range = new Random();
+        int lifeTime = 200;
+        public bool expired = false;
+
+        public Items()
+        {
+            item_image = Image.FromFile("items/item_01.png");
+
+            positionX = range.Next(10, 700);
+            positionY = range.Next(10, 500);
+
+            height = 50;
+            width = 50;
+
+
+        }
+
+        public void CheckLifeTime()
+        {
+
+            lifeTime--;
+            if (lifeTime < 1)
+            {
+                expired = true;
+            }
+
+        }
+
+
+
+
+    }
+}
